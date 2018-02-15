e2a8bfb1d75cbe3140acb938a316b8e676c05871
JackL <34636737+jackliuangel@users.noreply.github.com>
2018-02-16 01:13:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 
 var handleRequest = function(request, response) {
+  var os = require("os");
+  var hostname = os.hostname();
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('Hello World! from '+ hostname);
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
