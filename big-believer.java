c6ef21420d5911b816226efc1ee43e4d4bc9938a
JackL <34636737+jackliuangel@users.noreply.github.com>
2018-03-15 22:03:01 +0800

    Create big-believer.java
    dfs

---
+++ big-believer.java
@@ -0,0 +1,29 @@
+big believer of frequent release:
+
+	1. archtecure must change to double the release
+	2. fail fast to aglin with the user value
+	3. reduce the failure dependancy, suppose if this base is wrong....
+	4. user does not know the business value, until we put it in production and which is used most
+
+Finally, we can get feedback from user after each small release. That is how we interact with user.
+
+
+Metric Driven Development
+
+	1. transpanrency. make the goal for the team
+	2. cross-understaning of the metric. For example, double release with double resource?
+	3. monitor and keep open-minded: releases every 2 weeks? only weekend release?
+	4. you can do 5 release a day, but who many regression test?
+
+Metric drives behavivour.
+
+Pod model
+
+	1. must localized, not multi-nation
+	2. each one is full-stack, roles can be rotated, not static
+	3.  PO, BA, Developer are mandatory. But BA is requried ONLY when process change(BA is NOT just gathering what the user said)
+	4.  PO is used to signoff; prioritization
+	5. 6-8 is the best size of people
+
+
+
